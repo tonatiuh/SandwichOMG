632b38458f88fbfb23ac89c93c4e9b4b9672295f
Benjamin Iandavid <ian.rgz@gmail.com>
2012-07-25 10:53:48 -0700

    Finished social network integration

--- app/controllers/sandwiches_controller.rb
+++ app/controllers/sandwiches_controller.rb
@@ -14,18 +14,14 @@ class SandwichesController < ApplicationController
       if current_user
         @sandwich = current_user.sandwiches.build(params[:sandwich])
         if @sandwich.save
-          if params[:sandwich][:user_message].include? "sudo"
-            message = "Hey @generalthings I'm requesting a #{@sandwich.type} sandwich and i believe I added sudo to my message so get to work."
-          else
-            message = "Hi guys @generalthings can you please make me a #{@sandwich.type} sandwich, I will be forever thankfull."
-          end
-          Twitter.update(message)
-          redirect_to order_complete_path, notice: "We're taking care of buisnes here, we will notify you when your sandwich is ready."
+          Twitter.update(Sandwicher::Sandwich.build_message(params[:sandwich]))
+          redirect_to order_complete_path, notice: "We're taking care of busines here, we will notify you when your sandwich is ready."
         else
           render :new
         end
       else
-        redirect_to order_complete_path
+        Twitter.update(Sandwicher::Sandwich.build_message(params[:sandwich]))
+        redirect_to order_complete_path, notice: "We're taking care of busines here, we will notify you when your sandwich is ready."
       end
     else
       redirect_to angry_sandwicher_path
--- lib/sandwicher.rb
+++ lib/sandwicher.rb
@@ -10,5 +10,13 @@ module Sandwicher
         false
       end
     end
+    
+    def self.build_message user_message = ""
+      if user_message.include? "sudo"
+        return "Hey @generalthings I'm requesting a #{user_message[:type]} sandwich and i believe I added sudo to my message so get to work: #{user_message[:user_message]}"
+      else
+        return "Hi guys @generalthings can you please make me a #{user_message[:type]} sandwich, I will be forever thankfull: #{user_message[:user_message]}."
+      end
+    end
   end
 end
